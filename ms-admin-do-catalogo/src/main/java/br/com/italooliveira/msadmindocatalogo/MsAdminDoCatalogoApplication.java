1d496d559b814d81252bec617df8c44517e4bb57
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-06-12 11:57:52 -0300

    feat: scaffolding em catalogo do cliente

---
+++ ms-admin-do-catalogo/src/main/java/br/com/italooliveira/msadmindocatalogo/MsAdminDoCatalogoApplication.java
@@ -0,0 +1,13 @@
+package br.com.italooliveira.msadmindocatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdminDoCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdminDoCatalogoApplication.class, args);
+	}
+
+}
